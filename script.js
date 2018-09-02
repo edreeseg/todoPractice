3368c0a4e15fa6d707648949746ffc21d8580281
edreeseg <reeseg@ubhcorg.umdnj.edu>
2018-09-02 15:54:17 -0400

    Restructuring add/update Todos

--- script.js
+++ script.js
@@ -1,6 +1,68 @@
-class noTodoEntered extends Error {}
+class RequiredFieldsBlank extends Error {}
 
-class noPrioritySelected extends Error {}
+class DuplicateId extends Error {}
+
+const manageTodos = {
+	todos: [],
+	addTodos(){
+		const submitButton = document.getElementsByClassName("addTodo")[0];
+		const todoText = document.getElementById("todoEntry");
+		const priority = document.getElementsByName("priority");
+		const newTodo = {};
+		for(;;){
+			try {
+				const id = Number(Math.random().toString().substring(2, 10));
+				for (let entry of this.todos){
+					if (entry.id === id) throw new DuplicateId;
+				}
+				newTodo.id = id;
+				break;
+			} catch(e){
+				if (e instanceof DuplicateId){ 
+				continue;
+				}
+				else throw e;
+			}
+		}
+		if (todoText.value) newTodo.name = todoText.value;
+		else throw new RequiredFieldsBlank;
+		let check = true;
+		for (let level of priority){
+			if (level.checked){
+				newTodo.priority = level.value;
+				check = !check;
+			}
+		} if (check) throw new RequiredFieldsBlank;
+		this.todos.push(newTodo);
+		todoText.value = "";
+		this.updateTodos();
+	},
+	deleteTodos(e){
+		this.todos.splice(e.target.parentNode.id, 1);
+		console.log(this.todos);
+		this.updateTodos();
+	},
+	
+	updateTodos(){
+		const todoContainer = document.getElementsByClassName("todos")[0];
+		todoContainer.innerHTML = "";
+		for (let [index, entry] of this.todos.entries()){
+			const newTodo = document.createElement("li");
+			newTodo.textContent = entry.name;
+			newTodo.id = index;
+			if (entry.priority === "high") newTodo.classList.add("todoHigh");
+			else if (entry.priority === "medium") newTodo.classList.add("todoMedium");
+			else newTodo.classList.add("todoLow");
+			const exitButton = document.createElement("img");
+			exitButton.src = "https://i.imgur.com/DgrTvpF.png";
+			exitButton.classList.add("exitButton");
+			newTodo.appendChild(exitButton);
+			todoContainer.appendChild(newTodo);
+			manageEventListeners.deleteButtons();
+		}
+	}
+	
+}
 
 const manageEventListeners = {
 	todoDuration: (() => {
@@ -15,43 +77,15 @@ const manageEventListeners = {
 		}
 	})(),
 	
-	addTodo: (() => {
+	submitButton: (() => {
 		const submitButton = document.getElementsByClassName("addTodo")[0];
-		const todoText = document.getElementById("todoEntry");
-		const todoContainer = document.getElementsByClassName("todos")[0];
-		const priority = document.getElementsByName("priority");
-		const test = document.createElement("span");  // Change name!  Using span to have different max-width for text than for li.
-		const highPrio = (element) => element.style.flexGrow = "2";
-		submitButton.addEventListener("click", () => {
-			let todo = document.createElement("li");
-			if (todoText.value) test.textContent = todoText.value; // Used to be adding textContent to the li element directly.
-			else throw new noTodoEntered;
-			let check = true;
-			for (let level of priority){
-				if (level.checked){
-					if (level.value === "high") todo.classList.add("todoHigh");
-					else if (level.value === "medium") todo.classList.add("todoMedium");
-					else if (level.value === "low") todo.classList.add("todoLow");
-					check = !check;
-				}
-			} if (check) throw new noPrioritySelected;
-			todo.appendChild(test);  // Spans are getting replaced with each new entry.
-			const exitButton = document.createElement("img");
-			exitButton.src = "https://i.imgur.com/DgrTvpF.png";
-			exitButton.classList.add("exitButton");
-			todo.appendChild(exitButton);
-			todoContainer.appendChild(todo);
-			todoText.value = "";
-			manageEventListeners.deleteTodo();
-		});
+		submitButton.addEventListener("click", () => manageTodos.addTodos());
 	})(),
 	
-	deleteTodo: () => {
+	deleteButtons: () => { // Broken
 		const exitButtons = document.getElementsByClassName("exitButton");
 		for (let i = 0; i < exitButtons.length; i++){
-			exitButtons[i].addEventListener("click", (e) => {
-				e.target.parentNode.remove();
-			});
+			exitButtons[i].addEventListener("click", (e) => manageTodos.deleteTodos(e));
 		}
 	},
 	
